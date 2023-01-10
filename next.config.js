6336f73d1b1b63c023cd9d5514d67d31d0faae75
realmojo88 <strikers1999@naver.com>
2023-01-10 17:49:44 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["f5game.s3.ap-northeast-2.amazonaws.com"],
-  },
-};
-
-module.exports = nextConfig;
+module.exports = function(...args) {
+  let original = require('./next.config.original.1673338694187.js');
+  const finalConfig = {};
+  const target = { target: 'serverless' };
+  if (typeof original === 'function' && original.constructor.name === 'AsyncFunction') {
+    // AsyncFunctions will become promises
+    original = original(...args);
+  }
+  if (original instanceof Promise) {
+    // Special case for promises, as it's currently not supported
+    // and will just error later on
+    return original
+      .then((originalConfig) => Object.assign(finalConfig, originalConfig))
+      .then((config) => Object.assign(config, target));
+  } else if (typeof original === 'function') {
+    Object.assign(finalConfig, original(...args));
+  } else if (typeof original === 'object') {
+    Object.assign(finalConfig, original);
+  }
+  Object.assign(finalConfig, target);
+  return finalConfig;
+}
---
+++ next.config.original.1673338694187.js
@@ -0,0 +1,8 @@
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["f5game.s3.ap-northeast-2.amazonaws.com"],
+  },
+};
+
+module.exports = nextConfig;
