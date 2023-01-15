3cd55db32fb8a17d5b5ceb794ece9881769cfc38
realmojo <strikers1999@naver.com>
2023-01-15 18:00:55 +0900

    up

--- pages/main/[link].js
+++ pages/main/[link].js
@@ -1,60 +1,66 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Button, Input, Layout } from "antd";
+import { useRouter } from "next/router";
 import { AdsenseStart } from "../../components/Adsense/AdsenseStart";
 import { HeadComponent } from "../../components/Head";
 
 export default function Start({ item }) {
+  const router = useRouter();
   const [name, setName] = useState("");
   const onChangeName = (e) => {
     localStorage.setItem("f5game-test-name", e.target.value);
     setName(e.target.value);
   };
-  return (
-    <>
-      <HeadComponent
-        item={item}
-        canonicalUrl={`https://test.f5game.co.kr/main/${item.link}`}
-      />
-      <div
-        className="test-main-background"
-        style={{
-          backgroundColor: "transparent",
-          backgroundImage: `url(${item.logo})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          minHeight: 300,
-          width: "100%",
-          position: "absolute",
-        }}
-      ></div>
-      <main className="test-main relative">
-        <Layout className="test-layout">
-          <div className="test-logo">
-            <h1>{item.title}</h1>
-            <p>{item.description}</p>
-            <div className="mb-2 name-input">
-              <Input
-                size="large"
-                placeholder="이름 혹은 별칭을 입력해주세요."
-                value={name}
-                onChange={onChangeName}
-              />
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <>
+        <HeadComponent
+          item={item}
+          canonicalUrl={`https://test.f5game.co.kr/main/${item.link}`}
+        />
+        <div
+          className="test-main-background"
+          style={{
+            backgroundColor: "transparent",
+            backgroundImage: `url(${item.logo})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            minHeight: 300,
+            width: "100%",
+            position: "absolute",
+          }}
+        ></div>
+        <main className="test-main relative">
+          <Layout className="test-layout">
+            <div className="test-logo">
+              <h1>{item.title}</h1>
+              <p>{item.description}</p>
+              <div className="mb-2 name-input">
+                <Input
+                  size="large"
+                  placeholder="이름 혹은 별칭을 입력해주세요."
+                  value={name}
+                  onChange={onChangeName}
+                />
+              </div>
+              <AdsenseStart slotId={item.adsenses.main} />
+              <div className="text-center pt-2">
+                <a href={`/play/${item.link}`}>
+                  <Button type="primary" className="btn-start">
+                    시작하기
+                  </Button>
+                </a>
+              </div>
             </div>
-            <AdsenseStart slotId={item.adsenses.main} />
-            <div className="text-center pt-2">
-              <a href={`/play/${item.link}`}>
-                <Button type="primary" className="btn-start">
-                  시작하기
-                </Button>
-              </a>
-            </div>
-          </div>
-        </Layout>
-      </main>
-    </>
-  );
+          </Layout>
+        </main>
+      </>
+    );
+  }
 }
 
 export const getStaticPaths = async () => {
@@ -71,7 +77,7 @@ export const getStaticPaths = async () => {
   }
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 };
 
